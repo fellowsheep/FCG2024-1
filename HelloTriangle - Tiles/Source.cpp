ecc6b585e1e3e2ebe27a29c531f076d4ab278775
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2024-06-29 11:46:49 -0300

    Código atualizado para desenho do tilemap

--- HelloTriangle - Tiles/Source.cpp
+++ HelloTriangle - Tiles/Source.cpp
@@ -55,6 +55,8 @@ int dir = NONE;
 
 //Variáveis para armazenar as infos do tileset
 GLuint tilesetTexID;
+glm::vec2 offsetTex; //armazena o deslocamento necessário das coordenadas de textura no tileset
+GLuint VAOTile;
 int nTiles;
 glm::vec2 tileSize;
 
@@ -66,6 +68,8 @@ int tilemap[MAX_LINHAS][MAX_COLUNAS]; //este é o mapa de índices para os tiles
 
 //Função para fazer a leitura do tilemap do arquivo
 void loadMap(string fileName);
+GLuint setupTile();
+void drawMap(Shader &shader);
 
 // Fun��o MAIN
 int main()
@@ -165,15 +169,15 @@ int main()
 
 	//Leitura do tilemap
 	loadMap("map2.txt");
-
-
+	VAOTile = setupTile();
+	
 
 	//Habilita o shader que sera usado (glUseProgram)
 	shader.Use();
 
 
 	// Exercício 2 da Lista 2
-	glm::mat4 projection = glm::ortho(0.0, 800.0, 0.0, 600.0, -1.0, 1.0);
+	glm::mat4 projection = glm::ortho(0.0, 800.0, 600.0, 0.0, -1.0, 1.0);
 	//Enviando para o shader via variável do tipo uniform (glUniform....)
 	shader.setMat4("projection",glm::value_ptr(projection));
 
@@ -190,42 +194,13 @@ int main()
 		// Checa se houveram eventos de input (key pressed, mouse moved etc.) e chama as fun��es de callback correspondentes
 		glfwPollEvents();
 
-		//Verifica flags para movimentação do personagem
-		if (dir == LEFT)
-		{
-			player.moveLeft();
-		}
-		else if (dir == RIGHT)
-		{
-			player.moveRight();
-		}
-
-		player.getAABB();
-		coin.getAABB();
-
-		if (CheckCollision(player,coin) == true)
-		{
-			//cout << "Colidiu!" << endl;
-			//getchar();
-		}
+		
 
 		// Limpa o buffer de cor
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f); //cor de fundo
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		background.desenhar();
-		//ground.desenhar();
-		player.desenhar();
-		coin.moveItem();
-		coin.desenhar();
-		
-
-		//sprite3.desenhar();
-		//sprite4.desenhar();
-
-		//timer.finish();
-
-		//timer.wait(10);
+		drawMap(shader);
 
 		// Troca os buffers da tela
 		glfwSwapBuffers(window);
@@ -325,8 +300,10 @@ void loadMap(string fileName)
 	{
 		///leitura dos dados
 		string textureName;
+		int width, height;
 		//Leitura das informações sobre o tileset
 		arqEntrada >> textureName >> nTiles >> tileSize.y >> tileSize.x;
+		tilesetTexID = loadTexture(textureName, width, height);
 		//Só pra debug, printar os dados
 		cout << textureName << " " << nTiles << " " << tileSize.y << " " << tileSize.x << endl;
 		//Leitura das informações sobre o mapa (tilemap)
@@ -349,4 +326,100 @@ void loadMap(string fileName)
 	}
 }
 
+GLuint setupTile()
+{
+	GLuint VAO;
+	
+	offsetTex.s = 1.0/ (float) nTiles;
+	offsetTex.t = 1.0;
+	glm::vec3 cor; //temporario, dá pra tirar 
+	cor.r = 1.0;
+	cor.g = 0.0;
+	cor.b = 1.0;
+    //Especificação da geometria da sprite (quadrado, 2 triangulos)
+    GLfloat vertices[] = {
+		//x   y    z    r      g      b      s    t
+		-0.5, 0.5, 0.0, cor.r, cor.g, cor.b, 0.0, offsetTex.t, //v0
+        -0.5,-0.5, 0.0, cor.r, cor.g, cor.b, 0.0, 0.0, //v1
+         0.5, 0.5, 0.0, cor.r, cor.g, cor.b, offsetTex.s, offsetTex.t, //v2
+        -0.5,-0.5, 0.0, cor.r, cor.g, cor.b, 0.0, 0.0, //v1
+         0.5,-0.5, 0.0, cor.r, cor.g, cor.b, offsetTex.s, 0.0, //v3
+         0.5, 0.5, 0.0, cor.r, cor.g, cor.b, offsetTex.s, offsetTex.t  //v2
+	};
+
+	GLuint VBO;
+	//Gera��o do identificador do VBO
+	glGenBuffers(1, &VBO);
+	//Faz a conex�o (vincula) do buffer como um buffer de array
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//Envia os dados do array de floats para o buffer da OpenGl
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Gera��o do identificador do VAO (Vertex Array Object)
+	glGenVertexArrays(1, &VAO);
+	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de v�rtices
+	// e os ponteiros para os atributos 
+	glBindVertexArray(VAO);
+	//Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando: 
+	// Localiza��o no shader * (a localiza��o dos atributos devem ser correspondentes no layout especificado no vertex shader)
+	// Numero de valores que o atributo tem (por ex, 3 coordenadas xyz) 
+	// Tipo do dado
+	// Se est� normalizado (entre zero e um)
+	// Tamanho em bytes 
+	// Deslocamento a partir do byte zero 
+
+	//Atributo layout 0 - Posição - 3 valores dos 8 que descrevem o vértice
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	//Atributo layout 1 - Cor - 3 valores dos 8 que descrevem o vértice
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	//Atributo layout 2 - Coordenada de textura - 2 valores dos 8 que descrevem o vértice
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6* sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+
+	// Observe que isso � permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de v�rtice 
+	// atualmente vinculado - para que depois possamos desvincular com seguran�a
+	glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+	// Desvincula o VAO (� uma boa pr�tica desvincular qualquer buffer ou array para evitar bugs medonhos)
+	glBindVertexArray(0);
+	return VAO;
+}
+
+void drawMap(Shader &shader)
+{
+	shader.Use();
+
+	glm::vec2 posIni;
+	posIni.x = tileSize.x/2;
+	posIni.y = tileSize.y/2;
+
+	glBindTexture(GL_TEXTURE_2D, tilesetTexID); //Conectando com a textura
+   	glBindVertexArray(VAOTile); //Conectando ao buffer de geometria
+
+	for (int i=0; i < tilemapSize.y; i++)
+	{
+		for (int j=0; j < tilemapSize.x; j++)
+		{
+			//Atualiza a matriz de transformação do tile i j
+			glm::mat4 model = glm::mat4(1);
+			model = glm::translate(model,glm::vec3(posIni.x + j * tileSize.x, posIni.y + i * tileSize.y,0.0));
+			model = glm::scale(model, glm::vec3(tileSize.x,tileSize.y,1.0));
+			shader.setMat4("model", glm::value_ptr(model));
+
+			int indiceTile = tilemap[i][j];
+			shader.setVec2("offsetTex",indiceTile * offsetTex.x, offsetTex.y);
+			 
+			//Chamada de desenho para o tile
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+			
+		}
+
+	}
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindVertexArray(0);
+}
 
